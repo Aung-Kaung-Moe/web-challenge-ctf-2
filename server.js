f3beb1e31ecfecdeae61cb2eef9095a57f6fe97c
Aung-Kaung-Moe <aungkaungmoe@uit.edu.mm>
2026-02-17 17:23:33 +0630

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // ---- Challenge config ----
 const FLAG = process.env.FLAG || "flag{f4ke}";
-const WINDOW_MS = 80;  
+const WINDOW_MS = 50;  
 const REQUIRED = new Set(["f", "l", "a", "g"]);
 
 // In-memory per-session storage:
